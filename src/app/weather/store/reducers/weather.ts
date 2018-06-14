6fd7568d983b7d673018929df05cc3489b31e453
Roy Adams <roy@eden-grafix.co.uk>
2018-06-14 10:40:37 +0100

    update

--- src/app/weather/store/reducers/weather.ts
+++ src/app/weather/store/reducers/weather.ts
@@ -1,13 +1,17 @@
 import {Action} from "@ngrx/store";
 import {Cities} from '../../../model/weather'
 import { UPDATE_CITIES_ACTION, LOAD_CITIES_ACTION } from "../actions/weather";
+// import { AppState } from "../app.state";
 
 export function storeData(state: Cities[] = [], action:Action) : Cities[] {
     switch (action.type)  {
         case LOAD_CITIES_ACTION:
-        console.log(action)
-        return handleLoadCitiesAction(state, action);
+        // console.log(action)
+        return  handleLoadCitiesAction(state, action);
+     
+       
         case UPDATE_CITIES_ACTION:
+        console.log(state)
         return handleUpdateCitiesAction(state, action);
     default:
  
